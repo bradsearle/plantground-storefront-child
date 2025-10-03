cc84a3308ea23d52f286a1133ad2d8b130b49892
Brad.Searle <basearle@gmail.com>
2025-10-02 23:15:17 -0500

    Update content-product.php

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -14,7 +14,7 @@
  * (the theme developer) will need to copy the new files to your theme to
  * maintain compatibility. We try to do this as little as possible, but it does
  * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes
+ * the readme will list any important changes /
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
