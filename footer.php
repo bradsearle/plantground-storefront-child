f04eec6adfbbc4a5cc67818d4fcb10e41db66f5e
Brad.Searle <basearle@gmail.com>
2025-11-19 14:06:04 -0600

    testing format setting, added footer updates

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -9,46 +10,14 @@
 
 ?>
 
-    </div><!-- #content -->
-
-    <footer class="footer-grid">
-      
-      
-  <!-- Row 1 -->
-  <div class="footer-cell">About</div>
-  <div class="footer-cell">Terms</div>
-  <div class="footer-cell">Social</div>
-  
-  <!-- Row 2 -->
-  <div class="footer-cell">Company</div>
-  <div class="footer-cell">Shipping</div>
-  <div class="footer-cell">Instagram</div>
-  
-  <!-- Row 3 -->
-  <div class="footer-cell">Contact</div>
-  <div class="footer-cell">Privacy</div>
-  <div class="footer-cell">TikTok</div>
-  
-  <!-- Row 4 - Add your content later -->
-  <div class="footer-cell"></div>
-  <div class="footer-cell"></div>
-  <div class="footer-cell"></div>
-  
-  <!-- Row 5 - Add your content later -->
-  <div class="footer-cell"></div>
-  <div class="footer-cell"></div>
-  <div class="footer-cell"></div>
+<footer>jnfrinfernfoernfone</footer>
 
 
 
 
 
-
-    </footer><!-- .footer -->
-
-  </div><!-- #page -->
-
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 </body>
+
 </html>
