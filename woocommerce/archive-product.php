d7e0f2a1d5e3109e3553fe647835e7c4ebb19d5a
Brad.Searle <basearle@gmail.com>
2025-11-30 01:31:50 -0600

    mixin style updates

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -59,32 +59,33 @@ do_action('woocommerce_before_main_content');
       </div> <!-- /.flex-row -->
     </div><!-- /.shop-controls -->
   <?php endif; ?>
+  <div class="product-grid__container">
+    <?php
+    // The result count hook is commented out here as it's now handled above
+    do_action('woocommerce_output_all_notices');
+    // woocommerce_result_count(); // <-- This line is now redundant and removed/commented out
 
-  <?php
-  // The result count hook is commented out here as it's now handled above
-  do_action('woocommerce_output_all_notices');
-  // woocommerce_result_count(); // <-- This line is now redundant and removed/commented out
+    if (woocommerce_product_loop()) {
+      woocommerce_product_loop_start();
 
-  if (woocommerce_product_loop()) {
-    woocommerce_product_loop_start();
-
-    if (wc_get_loop_prop('total')) {
-      while (have_posts()) {
-        the_post();
-        do_action('woocommerce_shop_loop');
-        wc_get_template_part('content', 'product');
+      if (wc_get_loop_prop('total')) {
+        while (have_posts()) {
+          the_post();
+          do_action('woocommerce_shop_loop');
+          wc_get_template_part('content', 'product');
+        }
       }
+
+      woocommerce_product_loop_end();
+      do_action('woocommerce_after_shop_loop');
+    } else {
+      do_action('woocommerce_no_products_found');
     }
 
-    woocommerce_product_loop_end();
-    do_action('woocommerce_after_shop_loop');
-  } else {
-    do_action('woocommerce_no_products_found');
-  }
-
-  do_action('woocommerce_after_main_content');
-  do_action('woocommerce_sidebar');
-  ?>
+    do_action('woocommerce_after_main_content');
+    do_action('woocommerce_sidebar');
+    ?>
+  </div>
 
 
 </div><!-- /.site-main -->
