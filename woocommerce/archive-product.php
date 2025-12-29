bc8290ecd8f5cdc5c79830ca4a64430f60899d7a
Brad.Searle <basearle@gmail.com>
2025-12-28 21:40:49 -0600

    cd

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -22,12 +22,14 @@ do_action('woocommerce_before_main_content');
 
   <?php if (is_shop() || is_product_category()) : ?>
     <div class="shop-controls" id="shop-controls">
+
       <button class="mobile-filter-toggle" id="mobile-filter-toggle">
         <span id="filter-text">Show Filters</span>
         <svg id="filter-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
           <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
         </svg>
       </button>
+      <button class="shop__btn"> Shop Originals </button>
 
       <div class="shop-controls__row shop-controls__container" id="filters-container">
         <div class="shop-controls__left">
