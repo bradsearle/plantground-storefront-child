f63a0ef4ac75f38dec15f1baeb35337788ffde3f
Brad.Searle <basearle@gmail.com>
2025-12-29 22:42:23 -0600

    4t

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -19,19 +19,22 @@ do_action('woocommerce_before_main_content');
 
 <div class="site-main">
   <div class="custom-container">
-    <div class="hero-banner">
-      <h1 class="hero__title">Find your type</h1>
+    <div class="hero__copy">
+      <h1 class=" hero__title">Unique botanicals.</h1>
+    </div>
+    <div class="hero__copy-second">
+      <h2 class="hero__title">Find your type.</h2>
     </div>
   </div>
 
   <?php if (is_shop() || is_product_category()) : ?>
 
-    <button class="mobile-filter-toggle" id="mobile-filter-toggle">
-      <span id="filter-text">Show Filters</span>
-      <svg id="filter-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-        <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
-      </svg>
-    </button>
+    <div class="mobile-filter__container"> <button class="mobile-filter-toggle" id="mobile-filter-toggle">
+        <span id="filter-text">Show Filters</span>
+        <svg id="filter-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+          <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
+        </svg>
+      </button></div>
     <div class="shop-controls" id="shop-controls">
 
 
